ac9a517390e5308a3a12793de9f885c8e74ceca7
Jeff Sward <jas983@cornell.edu>
2021-11-23 14:34:07 -0500

    Added a function to create a quantile forecast from the BMA fit

--- enspp/bma.py
+++ enspp/bma.py
@@ -1,7 +1,9 @@
 from rpy2.robjects import pandas2ri
+import numpy as np
 import pandas as pd
+import xarray as xr
 
-from .util import _get_r_module, _attach_obs, _xr2pd
+from .util import _get_r_module, _attach_obs, _xr2pd, _fxda
 
 
 def fmt_training_data(wrfda, obsda):
@@ -34,7 +36,6 @@ def fmt_test_data(wrfda, obsda):
     return data
 
 
-
 def get_bma_fit(train_data):
     # Activate pandas2ri
     pandas2ri.activate()
@@ -46,3 +47,38 @@ def get_bma_fit(train_data):
     fit = gamma_bma.fit_bma(train_data, n_ens_members=5)
     
     return fit
+
+
+def bma_quantile_fx(fit, wrfda, obsda, gamma_bma=None, quantiles=np.arange(0.01, 1, 0.01)):
+    """
+    Create a quantile forcast using a previously-fit BMA model.  
+    """
+    if gamma_bma is None:
+        # Read the R gamma_bma module into Python
+        gamma_bma = _get_r_module('../R/gamma_bma.r', 'gamma_bma')
+
+    # This function is currently quite slow...
+    # Loop over the south_north dimension
+    for ii in range(0, len(wrfda.XLAT)):
+        # Loop over the west_east dimension
+        for jj in range(0, len(wrfda.XLONG)):
+            # Select data for only one location
+            wrfda_slice = wrfda.isel(south_north=ii, west_east=jj)
+            data_t = fmt_test_data(wrfda, obsda)
+            # Extract the quantiles using the BMA test fit
+            q = gamma_bma.quant_bma(fit, data_t, n_ens_members=5, quantiles=quantiles)
+            # Format the quantiles into a DataArray
+            fx_we_new = _fxda(q, wrfda_slice)
+            # Combine the DataArrays over the west_east dimension
+            if jj == 0:
+                fx_we = fx_we_new
+            else:
+                fx_we = xr.concat([fx_we, fx_we_new], 'west_east')
+        # Combine DataArrays over the south_north dimension
+        if ii == 0:
+            fx = fx_we
+        else:
+            fx = xr.concat([fx, fx_we], 'south_north')
+        
+    return fx
+    
