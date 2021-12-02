5f3e815bb759ebad983981d2c3e1bd4f51b72460
Jeff Sward <jas983@cornell.edu>
2021-12-02 10:43:41 -0500

    Added a notebook to reprocess some data files. Also, updated the format ensemble data example, and the example of how to format training and test data.

--- examples/example_fmt_ens_data.py
+++ examples/example_fmt_ens_data.py
@@ -5,33 +5,34 @@ import wrfpywind.data_preprocess as dp
 # Path to data
 datadir = '/share/mzhang/jas983/wrf_data/met4ene/wrfout/ARW/'
 
-# Specify the forecast initialization time
-t_init = '2019-12-10'
-t_init = pd.to_datetime(t_init)
+# Specify the start dates  
+start_dates = pd.date_range('2019-12-02', periods=7)
 
-# Start by formatting the directory
-d = t_init - pd.Timedelta(days=1)
-d_end = d + pd.Timedelta(days=4)
-dirs = util._get_ensdirs(d.strftime('%Y-%m-%d'))
+# Specify the end dates by specifying how long these simlulations should last
+end_dates = start_dates + pd.DateOffset(days=4)
 
-# The file produced after the basic postprocessing is named as 
-wo = f"ow_wrfout_d03_{d.strftime('%Y-%m-%d')}-{d_end.strftime('%Y-%m-%d')}"
+for ii in range(0, len(start_dates)):
+    print(start_dates[ii].strftime('%Y-%m-%d'))
+    dirs = util._get_ensdirs(start_dates[ii].strftime('%Y-%m-%d'))
 
-# Name for the outfile
-output_name = f"../data/ensds_{d.strftime('%Y%m%d')}-{d_end.strftime('%d')}.nc"
+    # The file produced after the basic postprocessing is named as 
+    wo = f"ow_wrfout_d03_{start_dates[ii].strftime('%Y-%m-%d')}-{end_dates[ii].strftime('%Y-%m-%d')}"
 
-# Read in data and put wind speed data from the ensemble into a new file.
-wrfens = dp.fmt_wrfens_wspd(wrfout_headdir=datadir, 
-                    wrfout_dirs=dirs,
-                    wrfout_files=[wo],
-                    model_names=[
-                                #  'Lee 2017',
-                                 'Draxl 2014a', 
-                                #  'Draxl 2014b', 
-                                 'Veron 2018', 
-                                 'Optis 2021'],
-                    heights=[20, 40, 60, 80, 100, 120, 140, 160, 180, 200],
-                    locations='all',
-                    save_nc=True, 
-                    nc_filename=output_name
-                    )
+    # Name for the outfile
+    output_name = f"../data/ensds_{start_dates[ii].strftime('%Y%m%d')}-{end_dates[ii].strftime('%d')}.nc"
+
+    # Read in data and put wind speed data from the ensemble into a new file.
+    wrfens = dp.fmt_wrfens_wspd(wrfout_headdir=datadir, 
+                        wrfout_dirs=dirs,
+                        wrfout_files=[wo],
+                        model_names=[
+                                    'Lee 2017',
+                                    'Draxl 2014a', 
+                                    'Draxl 2014b', 
+                                    'Veron 2018', 
+                                    'Optis 2021'],
+                        heights=[20, 40, 60, 80, 100, 120, 140, 160, 180, 200],
+                        locations='all',
+                        save_nc=True, 
+                        nc_filename=output_name
+                        )
